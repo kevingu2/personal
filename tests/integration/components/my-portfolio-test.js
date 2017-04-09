7c82cc05dcb0c409cc1ec1ac09c4d91d0e3c2952
Kevin Gu <kevin_gu@apple.com>
2017-04-08 21:18:03 -0700

    Hotfix for tests

--- tests/integration/components/my-portfolio-test.js
+++ tests/integration/components/my-portfolio-test.js
@@ -9,16 +9,16 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{my-portfolio}}`);
+  this.render(hbs`{{my-project}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#my-portfolio}}
+    {{#my-project}}
       template block text
-    {{/my-portfolio}}
+    {{/my-project}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '');
 });
