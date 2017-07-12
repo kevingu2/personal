3337117a3616bddcc4484f5be08f3e4c6030b16d
Kevin Gu <kevin_gu@apple.com>
2017-07-12 00:45:53 -0700

    Comment Change

--- tests/integration/components/my-portfolio-test.js
+++ tests/integration/components/my-portfolio-test.js
@@ -6,8 +6,6 @@ moduleForComponent('my-portfolio', 'Integration | Component | my portfolio', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{my-project}}`);
 
