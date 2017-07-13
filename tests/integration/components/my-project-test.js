6437589c2e610f33d6acacdc335704513c0ca387
Kevin Gu <kevin_gu@apple.com>
2017-07-13 04:54:12 -0700

    Test

--- tests/integration/components/my-project-test.js
+++ tests/integration/components/my-project-test.js
@@ -8,6 +8,7 @@ moduleForComponent('my-project', 'Integration | Component | my project', {
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  // Comments
 
   this.render(hbs`{{my-project}}`);
 
