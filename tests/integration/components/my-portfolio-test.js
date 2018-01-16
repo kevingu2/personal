a5fd0b8873acef08fcd3db0bb71746c8a65336a5
Kevin Gu <kevin_gu@apple.com>
2018-01-15 23:39:13 -0800

    Add portfolio-test

--- tests/integration/components/my-portfolio-test.js
+++ tests/integration/components/my-portfolio-test.js
@@ -5,6 +5,18 @@ moduleForComponent('my-portfolio', 'Integration | Component | my portfolio', {
   integration: true
 });
 
+let projects =[{
+  image:"image1",
+  link: "link1",
+  title: "title1",
+  detail: "detail1"
+},{
+  image:"image2",
+  link: "link2",
+  title: "title2",
+  detail: "detail2"
+}];
+
 test('it renders', function(assert) {
 
   this.render(hbs`{{my-portfolio}}`);
@@ -21,3 +33,10 @@ test('it renders', function(assert) {
   assert.equal(this.$().text().trim(), 'template block text');
 });
 
+test('it renders my portfolio', function(assert) {
+  this.set('projects', projects);
+  this.render(hbs`{{my-portfolio all_projects=projects}}`);
+
+  let itemElems = this.$().find('.item');
+  assert.equal(itemElems.length, projects.length);
+});
