dc43eb9d0accb3c73a138ffc2db035b569282d44
Kevin Gu <kevin_gu@apple.com>
2018-01-15 23:32:06 -0800

    Add work experience test

--- tests/integration/components/work-experience-test.js
+++ tests/integration/components/work-experience-test.js
@@ -5,6 +5,16 @@ moduleForComponent('work-experience', 'Integration | Component | work experience
   integration: true
 });
 
+let work_experience = {
+  title: "title1",
+  company: "company1",
+  duration: "2016 - present",
+  tasks: [
+    "task1",
+    "task2"
+  ]
+};
+
 test('it renders', function(assert) {
 
   // Set any properties with this.set('myProperty', 'value');
@@ -22,4 +32,19 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('it renders work experience', function(assert) {
+  this.set('work_experience', work_experience);
+  this.render(hbs`{{work-experience work_experience=work_experience}}`);
+
+  let h3Elems = this.$('.item').find('h3');
+  assert.equal(h3Elems.length, 1);
+  let h3Elem = h3Elems[0];
+  assert.ok(h3Elem.textContent.indexOf(work_experience.title) !== -1);
+  assert.ok(h3Elem.textContent.indexOf(work_experience.company) !== -1);
+  assert.ok(h3Elem.textContent.indexOf(work_experience.duration) !== -1);
+
+  let listElems = this.$('ul').find('li');
+  assert.equal(listElems.length, work_experience.tasks.length);
 });
